a6ed245d3e1326011ab50505eb27dcce812262f0
Jesus Quintero <jquintero@kuantum.cl>
2022-03-04 12:17:58 -0300

    Aplicando esLint

--- src/calculate/calculate.spec.ts
+++ src/calculate/calculate.spec.ts
@@ -1,54 +1,47 @@
-import calculate from './calculate'
+import calculate from './calculate';
 
 const mockObjValid = {
-   hr : 15,
-   place : 'TALCA',
-   weekDay : 'LUNES'
-}
+  hr: 15,
+  place: 'TALCA',
+  weekDay: 'LUNES',
+};
 
 const mockObjNotValid = {
-  hr : 1,
-  place : 'SANTIAGO',
-  weekDay : 'DOMINGO'
-}
+  hr: 1,
+  place: 'SANTIAGO',
+  weekDay: 'DOMINGO',
+};
 
 const validDay = 'LUNES';
 const validHr = 14;
 const validPlace = 'TALCA';
-const notValidHr = 25
+const notValidHr = 25;
 
-const communeWithQuarantine = ['SANTIAGO','MAIPU','RANCAGUA','VALPARAISO','COQUIMBO'];
+const communeWithQuarantine = [
+  'SANTIAGO',
+  'MAIPU',
+  'RANCAGUA',
+  'VALPARAISO',
+  'COQUIMBO',
+];
 const communeWithoutQuarantine = ['RENGO', 'TALCA', 'SAN FRANCISCO'];
-
-const weekDayArray = ['LUNES','MARTES','MIERCOLES','JUEVES','VIERNES'];
+const weekDayArray = ['LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES'];
 const weekendDayArray = ['SABADO', 'DOMINGO'];
 
 describe('Validate parameters', () => {
   test('Parameters: place, weekDay, must be a string. age must be a number', () => {
-    expect(mockObjValid.hr).toEqual(expect.any(Number))
-    expect(mockObjValid.place).toEqual(expect.any(String))
-    expect(mockObjValid.weekDay).toEqual(expect.any(String))
+    expect(mockObjValid.hr).toEqual(expect.any(Number));
+    expect(mockObjValid.place).toEqual(expect.any(String));
+    expect(mockObjValid.weekDay).toEqual(expect.any(String));
   });
   test('Parameter hr, must be a number between 0 and 23 and return true', () => {
     expect(mockObjValid.hr).toBeGreaterThanOrEqual(0);
     expect(mockObjValid.hr).toBeLessThanOrEqual(23);
-    expect(calculate(mockObjValid.hr, mockObjValid.place, mockObjValid.weekDay)).toBe(true);
+    expect(
+      calculate(mockObjValid.hr, mockObjValid.place, mockObjValid.weekDay),
+    ).toBe(true);
   });
-})
-
-//  describe('Validate quarantine place', () => { 
-//   test('If place are in quarantine response false', () => {
-//     const mockCallback = jest.fn(() => communeWithQuarantine.includes(mockObjNotValid.place));
-//     expect(mockCallback()).toBe(true);
-//     expect(calculate(15, mockObjNotValid.place, mockObjNotValid.weekDay)).toBe(false);
-//   });
-
-//   test('If place are in quarantine response true', () => {
-//     const mockCallback = jest.fn(() => communeWithQuarantine.includes(mockObjValid.place));
-//     expect(mockCallback()).toBe(false);
-//     expect(calculate(mockObjValid.hr, mockObjValid.place, mockObjValid.weekDay)).toBe(true);
-//   });
-// });
+});
 
 describe('Validate quarantine hr', () => {
   test('If hr is a number between 0 and 5 and return false', () => {
@@ -56,26 +49,33 @@ describe('Validate quarantine hr', () => {
     expect(mockObjNotValid.hr).toBeLessThanOrEqual(5);
     expect(calculate(mockObjNotValid.hr, validPlace, validDay)).toBe(false);
   });
-})
+});
 
-describe('Validate quarantine place', () => { 
-    test('If place are in quarantine response false', () => {
-      const isQuarantinePlace = () => communeWithQuarantine.includes(mockObjNotValid.place);
-      expect(isQuarantinePlace()).toBe(true);
-      expect(calculate(validHr, mockObjNotValid.place, validDay)).toBe(false);
-    });
-
-    test('If place are in quarantine response true', () => {
-      const isQuarantinePlace = () => communeWithQuarantine.includes(mockObjValid.place);
-      expect(isQuarantinePlace()).toBe(false);
-      expect(calculate(mockObjValid.hr, mockObjValid.place, mockObjValid.weekDay)).toBe(true);
-    });
+describe('Validate quarantine place', () => {
+  test('If place are in quarantine response false', () => {
+    const isQuarantinePlace = () =>
+      communeWithQuarantine.includes(mockObjNotValid.place);
+    expect(isQuarantinePlace()).toBe(true);
+    expect(calculate(validHr, mockObjNotValid.place, validDay)).toBe(false);
   });
+
+  test('If place are in quarantine response true', () => {
+    const isQuarantinePlace = () =>
+      communeWithQuarantine.includes(mockObjValid.place);
+    expect(isQuarantinePlace()).toBe(false);
+    expect(
+      calculate(mockObjValid.hr, mockObjValid.place, mockObjValid.weekDay),
+    ).toBe(true);
+  });
+});
+
 describe('Validate quarantine week day', () => {
   test('If is a week day, should be response true', () => {
     const isWeekendDay = () => weekendDayArray.includes(mockObjValid.weekDay);
     expect(isWeekendDay()).toBe(false);
-    expect(calculate(mockObjValid.hr, mockObjValid.place, mockObjValid.weekDay)).toBe(true);
+    expect(
+      calculate(mockObjValid.hr, mockObjValid.place, mockObjValid.weekDay),
+    ).toBe(true);
   });
 
   test('If is a weekend day, should be response false', () => {
@@ -87,10 +87,14 @@ describe('Validate quarantine week day', () => {
 
 describe('Validate function and response true or false according to implementation ', () => {
   test('Validate correct', () => {
-    expect(calculate(mockObjValid.hr, mockObjValid.place, mockObjValid.weekDay)).toBe(true);
+    expect(
+      calculate(mockObjValid.hr, mockObjValid.place, mockObjValid.weekDay),
+    ).toBe(true);
   });
-  
+
   test('Validate incorrect', () => {
-    expect(calculate(notValidHr, mockObjNotValid.place, mockObjNotValid.weekDay)).toBe(false);
+    expect(
+      calculate(notValidHr, mockObjNotValid.place, mockObjNotValid.weekDay),
+    ).toBe(false);
   });
 });
--- src/calculate/calculate.ts
+++ src/calculate/calculate.ts
@@ -1,26 +1,28 @@
-const weekDayArray = ['LUNES','MARTES','MIERCOLES','JUEVES','VIERNES'];
-const weekendDayArray = ['SABADO', 'DOMINGO']
+const weekDayArray = ['LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES'];
+const weekendDayArray = ['SABADO', 'DOMINGO'];
 
-const covid40OK = ['SANTIAGO','MAIPU','RANCAGUA','VALPARAISO','COQUIMBO']
-const covid40 = ['RENGO', 'TALCA', 'SAN FRANCISCO', 'PUNTA ARENAS']
+const covid40OK = ['SANTIAGO', 'MAIPU', 'RANCAGUA', 'VALPARAISO', 'COQUIMBO'];
+const covid40 = ['RENGO', 'TALCA', 'SAN FRANCISCO', 'PUNTA ARENAS'];
 
-function calculateOption(hr, place, weekDay) {
-    if(hr < 0 || hr > 23) { // is a real hr
-      return false
-    }
-
-    if(hr < 5){ // is a valid permission hr
-      return false
-    }
-
-    if(weekendDayArray.includes(weekDay) === true){
-      return false
-    }
-
-    if(covid40OK.includes(place) === true){
-      return false
-    }
-
-    return true;
+function calculateOption(hr: number, place: string, weekDay: string) {
+  if (hr < 0 || hr > 23) {
+    // is a real hr
+    return false;
   }
-  export default calculateOption
+
+  if (hr < 5) {
+    // is a valid permission hr
+    return false;
+  }
+
+  if (weekendDayArray.includes(weekDay) === true) {
+    return false;
+  }
+
+  if (covid40OK.includes(place) === true) {
+    return false;
+  }
+
+  return true;
+}
+export default calculateOption;
